c5215704e0e089734ddb0020867bdce1c68fbc97
Alnoman141 <nmnaba14@gmail.com>
2022-10-12 02:07:44 +0600

    server created done

--- index.js
+++ index.js
@@ -1,2 +1,33 @@
-const hello = 'Hello world';
-console.log(hello);
+/**
+ * Name: NodeJs Uptime monitoring app
+ * Description: A simple nodejs uptime monitoring app for practice nodejs
+ * Author: Noman
+ * Date: 12.10.2022
+ */
+
+// Dependencies
+const http = require('http');
+
+// app object - module scafollding
+const app = {};
+
+// configarations
+app.config = {
+    port: 3000,
+};
+
+// create server
+app.createServer = () => {
+    const server = http.createServer(app.handleReqRes);
+    server.listen(app.config.port, () => {
+        console.log(`server is listen on port ${app.config.port}`);
+    });
+};
+
+// handle requiest and response
+app.handleReqRes = (req, res) => {
+    res.end('Hello programmers, How are you?');
+};
+
+// start the server
+app.createServer();
